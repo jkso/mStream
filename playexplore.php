cf363304086a14db865067dab77d579dc5f824f3
Paul Sori <paul@ufn.com>
2013-11-29 08:34:15 -0500

    Playlist sorting

--- playexplore.php
+++ playexplore.php
@@ -1,3 +1,95 @@
+<!-- jQuery -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+<script>
+/*
+ * HTML5 Sortable jQuery Plugin
+ * http://farhadi.ir/projects/html5sortable
+ * 
+ * Copyright 2012, Ali Farhadi
+ * Released under the MIT license.
+ */
+(function($) {
+var dragging, placeholders = $();
+$.fn.sortable = function(options) {
+        var method = String(options);
+        options = $.extend({
+                connectWith: false
+        }, options);
+        return this.each(function() {
+                if (/^enable|disable|destroy$/.test(method)) {
+                        var items = $(this).children($(this).data('items')).attr('draggable', method == 'enable');
+                        if (method == 'destroy') {
+                                items.add(this).removeData('connectWith items')
+                                        .off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s');
+                        }
+                        return;
+                }
+                var isHandle, index, items = $(this).children(options.items);
+                var placeholder = $('<' + (/^ul|ol$/i.test(this.tagName) ? 'li' : 'div') + ' class="sortable-placeholder">');
+                items.find(options.handle).mousedown(function() {
+                        isHandle = true;
+                }).mouseup(function() {
+                        isHandle = false;
+                });
+                $(this).data('items', options.items)
+                placeholders = placeholders.add(placeholder);
+                if (options.connectWith) {
+                        $(options.connectWith).add(this).data('connectWith', options.connectWith);
+                }
+                items.attr('draggable', 'true').on('dragstart.h5s', function(e) {
+                        if (options.handle && !isHandle) {
+                                return false;
+                        }
+                        isHandle = false;
+                        var dt = e.originalEvent.dataTransfer;
+                        dt.effectAllowed = 'move';
+                        dt.setData('Text', 'dummy');
+                        index = (dragging = $(this)).addClass('sortable-dragging').index();
+                }).on('dragend.h5s', function() {
+                        if (!dragging) {
+                                return;
+                        }
+                        dragging.removeClass('sortable-dragging').show();
+                        placeholders.detach();
+                        if (index != dragging.index()) {
+                                dragging.parent().trigger('sortupdate', {item: dragging});
+                        }
+                        dragging = null;
+                }).not('a[href], img').on('selectstart.h5s', function() {
+                        this.dragDrop && this.dragDrop();
+                        return false;
+                }).end().add([this, placeholder]).on('dragover.h5s dragenter.h5s drop.h5s', function(e) {
+                        if (!items.is(dragging) && options.connectWith !== $(dragging).parent().data('connectWith')) {
+                                return true;
+                        }
+                        if (e.type == 'drop') {
+                                e.stopPropagation();
+                                placeholders.filter(':visible').after(dragging);
+                                dragging.trigger('dragend.h5s');
+                                return false;
+                        }
+                        e.preventDefault();
+                        e.originalEvent.dataTransfer.dropEffect = 'move';
+                        if (items.is(this)) {
+                                if (options.forcePlaceholderSize) {
+                                        placeholder.height(dragging.outerHeight());
+                                }
+                                dragging.hide();
+                                $(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
+                                placeholders.not(placeholder).detach();
+                        } else if (!placeholders.is(this) && !$(this).children(options.items).length) {
+                                placeholders.detach();
+                                $(this).append(placeholder);
+                        }
+                        return false;
+                });
+        });
+};
+})(jQuery);
+</script>
+
 <?php
 session_start();
 if($_SESSION["login"]==1){
@@ -14,9 +106,7 @@ else{
 
 <link rel="stylesheet" type="text/css" href="css/playexplore.css" />
 
-<!-- jQuery -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
 
 <!-- The following are all for jplayer -->
 <link href="jPlayer24/skin/prettify-jPlayer.css" rel="stylesheet" type="text/css" />
@@ -94,12 +184,14 @@ $(document).ready(function(){
 		// 	title: addfile,
 		// 	mp3: mp3location
 		// });
-	$('ul#playlist').append(
+		$('ul#playlist').append(
 		    $('<li/>', {
 		        'data-songurl': mp3location,
+		        'class': 'dragable',
 		        html: addfile
 		    })
 		);
+		$('#playlist').sortable();
 	});
 
 // when you click 'add directory', add entire directory to the playlist
@@ -289,7 +381,7 @@ $(document).ready(function(){
 		$('#jquery_jplayer_1').jPlayer("play");
 	});
 
-
+	$('#playlist').sortable();
 
 
 });
@@ -318,9 +410,9 @@ $(document).ready(function(){
 
 	<div id="playlist_container">
 		<ul id="playlist">
-			<li data-songurl='/audiofiles/thethec.mp3' class="current">Song1</li>
-			<li data-songurl='/audiofiles/Epiccopy.mp3'>Song2</li>
-			<li data-songurl='/audiofiles/Epiccopy.mp3' >Song3</li>
+			<li data-songurl='/audiofiles/thethec.mp3' class="current dragable">Song1</li>
+			<li data-songurl='/audiofiles/Epiccopy.mp3' class="dragable">Song2</li>
+			<li data-songurl='/audiofiles/Epiccopy.mp3' class="dragable">Song3</li>
 		</ul>
 	</div>
 
