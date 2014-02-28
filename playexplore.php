929d4b870f136fe8e6da53df19f3781bfae33541
James <james@ufn.com>
2014-02-28 00:17:32 -0500

    Icons
    Favicon and apple touch icons

--- playexplore.php
+++ playexplore.php
@@ -15,6 +15,11 @@
     <meta content="yes" name="mobile-web-app-capable">
 	<meta content="yes" name="apple-mobile-web-app-capable">
 	<meta content="black" name="apple-mobile-web-app-status-bar-style">
+	
+	<link rel="apple-touch-icon" href="img/apple-icon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon/apple-touch-icon-152x152.png">
 
     <title>Mstream Media Player - All your media. Everywhere you go.</title>
     <?php include('includes/assets.inc.php'); ?>
